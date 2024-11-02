a55ea17988836f1ef20eae3871dac41a9fb87910
KingNotNgu <th491725@gmail.com>
2024-11-03 00:00:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg i python-pip && yes | pip install requests asyncio aiohttp psutil prettytable
+yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg i python-pip && yes | pip install requests asyncio aiohttp psutil prettytable Flask colorama crypto pycryptodome
